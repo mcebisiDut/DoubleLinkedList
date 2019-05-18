eca919484e34bce89c2c3d8808952382202ae2f3
mcebisi <mcebisimkhohliwe@gmail.com>
2019-05-18 14:48:29 +0200

    Added ListNode

---
+++ ListNode.java
@@ -0,0 +1,39 @@
+public class ListNode<T> {
+    private T item;
+    private ListNode<T> next;
+    private ListNode<T> previous;
+
+    ListNode(T item, ListNode next, ListNode previous) {
+        SetItem(item);
+        SetNext(next);
+        SetPrevious(previous);
+    }
+
+    ListNode(T item) {
+        SetItem(item);
+    }
+
+    public void SetItem(T item){
+        this.item = item;
+    }
+
+    public T GetItem(){
+        return item;
+    }
+
+    public void SetNext(ListNode next){
+        this.next = next;
+    }
+
+    public ListNode<T> GetNext(){
+        return next;
+    }
+
+    public void SetPrevious(ListNode previous){
+        this.previous = previous;
+    }
+
+    public ListNode<T> GetPrevious(){
+        return previous;
+    }
+}
